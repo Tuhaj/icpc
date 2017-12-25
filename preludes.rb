01bad305c1dad385f56f849766fe5b0c90535097
Piotr Zientara <314zientara@gmail.com>
2017-12-25 15:09:20 +0100

    tests solved

--- preludes.rb
+++ preludes.rb
@@ -1,5 +1,44 @@
-module Preludes
-  def alt_name(name)
-    name
+class Preludes
+
+  def initialize(key)
+    @key = key
+    key_parts = @key.split(' ')
+    @key_note = key_parts[0]
+    @tonality = key_parts[1]
+  end
+
+  NOTES = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+
+  INVERSE_ACCIDENTALS = {
+    '#' => 'b',
+    'b' => '#'
+  }
+
+  def has_accidental?
+    INVERSE_ACCIDENTALS.keys.include?(@key_note[-1])
+  end
+
+  def swap_accidental
+    @key_note[-1] = INVERSE_ACCIDENTALS[@key_note[-1]]
+  end
+
+  def change_note
+    if @key_note[-1] == '#'
+      step = 1
+    else
+      step = -1
+    end
+    new_note_index = NOTES.find_index(@key_note[0]) + step
+    @key_note[0] = NOTES[(new_note_index) % NOTES.size]
+  end
+
+  def alt_name
+    if has_accidental?
+      change_note
+      swap_accidental
+      "#{@key_note} #{@tonality}"
+    else
+      'UNIQUE'
+    end
   end
 end
--- preludes.spec.rb
+++ preludes.spec.rb
@@ -3,6 +3,24 @@ require_relative 'preludes'
 
 describe 'alt_name' do
   it 'returns correct alt name' do
-    expect(Preludes.alt_name('A♭ minor')).to eq 'G♯ major'
+    expect(Preludes.new('Ab minor').alt_name).to eq 'G# minor'
+  end
+
+  it "doesn't return wrong alt name" do
+    expect(Preludes.new('A♭ minor').alt_name).not_to eq 'A♭ minor'
+  end
+end
+
+describe 'icpc test cases' do
+  it 'returns correct input for Ab minor' do
+    expect(Preludes.new('Ab minor').alt_name).to eq 'G# minor'
+  end
+
+  it 'returns correct input for D# major' do
+    expect(Preludes.new('D# major').alt_name).to eq 'Eb major'
+  end
+
+  it 'returns correct input for G minor' do
+    expect(Preludes.new('G minor').alt_name).to eq 'UNIQUE'
   end
 end
