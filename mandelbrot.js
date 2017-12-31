f5324cb832d49659a4887ca9ed29a81305378985
Piotr Zientara <314zientara@gmail.com>
2017-12-31 18:05:04 +0100

    style fixes for Mandelbrot

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,22 +1,22 @@
-const rootFromSumOfSquares = (a, b) => {
-  return Math.sqrt((a * a) + (b * b));
-};
-
+const square = num => Math.pow(num, 2);
+const sqrtFromSumOfSquares = (a, b) => ( Math.sqrt( square(a) + square(b) ) );
 
 const isInSet = (a, b, maxIterations) => {
-  DIVERGE_LIMIT = 2;
-  var modulus;
-  var realPart = 0;
-  var iPart = 0;
+  const DIVERGE_LIMIT = 2;
+  let modulus,
+      realPart = 0,
+      iPart = 0;
 
-  for(var index = 0; index < maxIterations; index++) {
-    realPart = realPart + a;
-    iPart = iPart + b;
-    modulus = rootFromSumOfSquares(realPart, iPart);
+  for(let index = 0; index < maxIterations; index++) {
+    realPart += a;
+    iPart += b;
+    modulus = sqrtFromSumOfSquares(realPart, iPart);
+
     if(modulus >= DIVERGE_LIMIT) {
       return 'OUT';
     }
-    realPart = Math.pow(realPart, 2) - Math.pow(iPart, 2);
+
+    realPart = square(realPart) - square(iPart);
     iPart = 2 * realPart * iPart;
   }
   return 'IN'
