59d436a1283a940509d4b6f758dbfa1c6db90717
Piotr Zientara <314zientara@gmail.com>
2017-12-31 17:53:49 +0100

    solution for Mandelbrot

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,5 +1,25 @@
-const isInSet = (real1, real2, integer) => {
-  return 'OUT'
+const rootFromSumOfSquares = (a, b) => {
+  return Math.sqrt((a * a) + (b * b));
+};
+
+
+const isInSet = (a, b, maxIterations) => {
+  DIVERGE_LIMIT = 2;
+  var modulus;
+  var realPart = 0;
+  var iPart = 0;
+
+  for(var index = 0; index < maxIterations; index++) {
+    realPart = realPart + a;
+    iPart = iPart + b;
+    modulus = rootFromSumOfSquares(realPart, iPart);
+    if(modulus >= DIVERGE_LIMIT) {
+      return 'OUT';
+    }
+    realPart = Math.pow(realPart, 2) - Math.pow(iPart, 2);
+    iPart = 2 * realPart * iPart;
+  }
+  return 'IN'
 };
 
 exports.isInSet = isInSet;
